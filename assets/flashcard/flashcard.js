3d25e8595fece87d670f2047246486ee4e1d8283
Temi-Tade <akintadetemi19@gmail.com>
2024-08-29 16:34:43 +0100

    edit, delete, navigation, progress, count features implemented

--- assets/flashcard/flashcard.js
+++ assets/flashcard/flashcard.js
@@ -1,12 +1,20 @@
+let index = 0;
+
 sessionStorage.setItem("ace-it-data", JSON.stringify({
     name: undefined,
     desc: ""
-}))
+}));
 
 const TOGGLE_FORMS = (e, el, other) => {
-    e.preventDefault()
-    el.style.display = "none"
-    other.style.display = "flex"
+    e.preventDefault();
+    if (window.innerWidth > 420) {
+        el.style.display = "none";
+        other.style.display = "flex";
+    }else{
+        el.style.display = "none";
+        other.style.display = "block";
+    }
+
     let sessionData = JSON.parse(sessionStorage.getItem("ace-it-data"))
     document.querySelector("#info h2").innerHTML = sessionData.name
     document.querySelector("#info em").innerHTML = sessionData.desc
@@ -51,38 +59,64 @@ const CREATE_MODAL = (text) => {
 
     window.onclick = (ev) => {
         if (ev.target === document.querySelector("#modalbg")) {
-            document.querySelector("#modalbg").style.display = "none"
+            document.querySelector("#modalbg").animate({
+                opacity: ["1", "0"],
+            }, {
+                iterations: 1,
+                duration: 500
+            })
+            setTimeout(() => {
+                document.querySelector("#modalbg").style.display = "none"
+            }, 490);
         }
     }
 }
 
-const ADD_CARD = () => {
-    let angle = 0
-    document.querySelector("#preview").innerHTML = ""
-    cards.forEach((val, ind, arr) => {
-        document.querySelector("#preview").innerHTML = `
-            <div class='card-wrap'>
-                <div class="card">
-                    <p class="card-text">${arr[0].term}</p>
-                </div>
+const PREVIEW_CARD = (index) => {
+    document.querySelector("#preview").innerHTML = `
+        <div class='card-wrap'>
+            <div class="card">
+                <p class="card-text">${index}</p>
             </div>
-        `
-    });
-    [...document.querySelectorAll(".card")].forEach((val, ind, arr) => {
+        </div>
+    `
+}
+
+const ADD_CARD = () => {
+    index = 0;
+    document.querySelector("#preview").innerHTML = "";
+    PREVIEW_CARD(cards[0].term);
+    FLASHCARD_COUNT();
+    SET_PROGRESS();
+    [...document.querySelectorAll(".card")].forEach((val) => {
         val.onclick = () => {
-            angle += 360
-            val.style.transform = `rotateY(${angle}deg)`
-            if (val.querySelector(".card-text").innerHTML === cards[ind].term) {
-                val.querySelector(".card-text").innerHTML = cards[ind].def
+            val.animate({
+                transform: ["rotateY(-180deg)", "rotateY(0deg)"]
+            }, {
+                duration: 250,
+                iterations: 1
+            })  
+            if (val.querySelector(".card-text").innerHTML === cards[0].term) {
+                val.querySelector(".card-text").innerHTML = cards[0].def;
             } else {
-                val.querySelector(".card-text").innerHTML = cards[ind].term
+                val.querySelector(".card-text").innerHTML = cards[0].term;
+            }
+        }
+
+        val.oncontextmenu = (e) => {
+            e.preventDefault();
+            document.querySelector("#contextmenu").style = `display: block; top: ${e.offsetY}px; left: ${e.offsetX - 200}px`;
+            window.onclick = (e) => {
+                if (e.target !== document.querySelector("#contextmenu")) {
+                    document.querySelector("#contextmenu").style.display = "none";
+                }
             }
         }
     })
 }
 
 //code for flashcard starts here //
-const cards = []
+const cards = [];
 
 class Flashcard{
     constructor(term, def){
@@ -96,20 +130,159 @@ class Flashcard{
         })
     }
     delete(ind){
-        cards.splice(ind, 1)
+        cards.splice(ind, 1);
     }
-    edit(){
-        CREATE_MODAL("")
+    edit(i){
+        CREATE_MODAL(`
+            <h3>Edit flashcard</h3>
+            <form autocomplete="off" spellcheck="false">
+                ${document.querySelector("#flashcard-data form").innerHTML}
+            </form>
+        `);
+        document.querySelector("#modal form button").textContent = "Save Changes";
+        document.querySelector("#modal form #term").value = cards[i].term;
+        document.querySelector("#modal form #def").value = cards[i].def;
+        document.querySelector("#modal form").onsubmit = (e) => {
+            e.preventDefault();
+            cards[i] = new Flashcard(document.querySelector("#modal form #term").value, document.querySelector("#modal form #def").value);
+            ADD_CARD()
+        }
     }
 }
 
-document.querySelector("#flashcard-data form").onsubmit = (ev, term, def) => {
-    ev.preventDefault()
-    let card = new Flashcard(document.querySelector("#flashcard-data form #term").value, document.querySelector("#flashcard-data form #def").value)
-    cards.push(card)
-    ADD_CARD()
-    console.log(cards)
-    document.querySelector("#flashcard-data form").reset()
+document.querySelector("#flashcard-data form").onsubmit = (ev) => {
+    ev.preventDefault();
+    if (window.innerWidth <= 420) {
+        TOGGLE_FORMS(ev, document.querySelector("#flashcard-data form"), document.querySelector("#flashcard-preview"));
+    }
+    let card = new Flashcard(document.querySelector("#flashcard-data form #term").value, document.querySelector("#flashcard-data form #def").value);
+    card.add();
+    ADD_CARD();
+    console.log(cards);
+    document.querySelector("#flashcard-data form").reset();
+
+    document.querySelector("#edit-flashcard-btn").onclick = () => {
+        card.edit(index);
+    }
+
+    document.querySelector("#contextmenu button").onclick = () => {
+        card.delete(index);
+        PREVIEW_CARD(cards[0].term);
+        FLASHCARD_COUNT();
+        SET_PROGRESS();
+        [...document.querySelectorAll(".card")].forEach((val) => {
+            val.onclick = () => {
+                val.animate({
+                    transform: ["rotateY(-180deg)", "rotateY(0deg)"]
+                }, {
+                    duration: 250,
+                    iterations: 1
+                })  
+                if (val.querySelector(".card-text").innerHTML === cards[index].term) {
+                    val.querySelector(".card-text").innerHTML = cards[index].def;
+                } else {
+                    val.querySelector(".card-text").innerHTML = cards[index].term;
+                }
+            }
+
+            val.oncontextmenu = (e) => {
+                e.preventDefault();
+                document.querySelector("#contextmenu").style = `display: block; top: ${e.offsetY}px; left: ${e.offsetX - 200}px`;
+                window.onclick = (e) => {
+                    if (e.target !== document.querySelector("#contextmenu")) {
+                        document.querySelector("#contextmenu").style.display = "none";
+                    }
+                }
+            }
+        })
+        console.log(cards);
+    }
     //define preview
     //implement edit, next, prev
+}
+
+const GO_TO_PREV_CARD = () => {
+    try{
+        index--;
+        if (index < 0) {
+            index = 0;
+        }
+        PREVIEW_CARD(cards[index].term);
+        FLASHCARD_COUNT();
+        SET_PROGRESS();
+        [...document.querySelectorAll(".card")].forEach((val) => {
+            val.onclick = () => {
+                val.animate({
+                    transform: ["rotateY(-180deg)", "rotateY(0deg)"]
+                }, {
+                    duration: 250,
+                    iterations: 1
+                })  
+                if (val.querySelector(".card-text").innerHTML === cards[index].term) {
+                    val.querySelector(".card-text").innerHTML = cards[index].def;
+                } else {
+                    val.querySelector(".card-text").innerHTML = cards[index].term;
+                }
+            }
+
+            val.oncontextmenu = (e) => {
+                e.preventDefault();
+                document.querySelector("#contextmenu").style = `display: block; top: ${e.offsetY}px; left: ${e.offsetX - 200}px`;
+                window.onclick = (e) => {
+                    if (e.target !== document.querySelector("#contextmenu")) {
+                        document.querySelector("#contextmenu").style.display = "none";
+                    }
+                }
+            }
+        })
+    }catch{
+        CREATE_MODAL('OOPS... You have not added any flashcards yet.');
+    }
+}
+
+const GO_TO_NEXT_CARD = () => {
+    try{
+        index++;
+        if (index > cards.length - 1) {
+            index = cards.length - 1;
+        }
+        PREVIEW_CARD(cards[index].term);
+        SET_PROGRESS();
+        FLASHCARD_COUNT();
+        [...document.querySelectorAll(".card")].forEach((val, ind) => {
+            val.onclick = () => {
+                val.animate({
+                    transform: ["rotateY(-180deg)", "rotateY(0deg)"]
+                }, {
+                    duration: 250,
+                    iterations: 1
+                })  
+                if (val.querySelector(".card-text").innerHTML === cards[index].term) {
+                    val.querySelector(".card-text").innerHTML = cards[index].def;
+                } else {
+                    val.querySelector(".card-text").innerHTML = cards[index].term;
+                }
+            }
+
+            val.oncontextmenu = (e) => {
+                e.preventDefault();
+                document.querySelector("#contextmenu").style = `display: block; top: ${e.offsetY}px; left: ${e.offsetX - 200}px`;
+                window.onclick = (e) => {
+                    if (e.target !== document.querySelector("#contextmenu")) {
+                        document.querySelector("#contextmenu").style.display = "none";
+                    }
+                }
+            }
+        })
+    }catch{
+        CREATE_MODAL('OOPS... You have not added any flashcards yet.');
+    }
+}
+
+const FLASHCARD_COUNT = () => {
+    document.querySelector("#flashcard-count").innerHTML = `${index+1} of ${cards.length}`;
+}
+
+const SET_PROGRESS = () => {
+    document.querySelector("#level").style.width = `${((index + 1) / cards.length) * 100}%`
 }
